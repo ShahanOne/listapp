12dfcfb57eb0f7e121b076beadad135b7192308c
Shahan.One <shahanhussain9105@gmail.com>
2023-06-16 22:18:13 +0530

    authentication and err msg

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,4 +1,18 @@
+import { useState } from 'react';
+
 function Login(props) {
+  const [userName, setUserName] = useState('');
+  const [password, setPassword] = useState('');
+
+  function handleUserName(e) {
+    const { value } = e.target;
+    setUserName(value);
+  }
+  function handlePassword(e) {
+    const { value } = e.target;
+    setPassword(value);
+  }
+
   return (
     <div className="grid md:grid-cols-2 px-2 py-40 md:px-80 md:py-44">
       <div className=" text-white  bg-indigo-600 text-3xl font-bold  rounded-lg shadow-lg pl-16 py-16 md:py-44">
@@ -12,6 +26,8 @@ function Login(props) {
         <input
           className="my-2 block w-3/4 rounded bg-gray-200"
           type="text"
+          value={userName}
+          onChange={handleUserName}
           id="userName"
         />
 
@@ -21,11 +37,17 @@ function Login(props) {
         <input
           className="my-2 block w-3/4 rounded bg-gray-200"
           type="password"
+          value={password}
+          onChange={handlePassword}
           id="password"
         />
 
         <button
-          onClick={props.clicked}
+          onClick={() =>
+            !(userName && password)
+              ? alert('please fill in the required fields')
+              : props.clicked()
+          }
           className="rounded block shadow-lg my-4 py-2 px-4 text-white bg-indigo-600 hover:bg-indigo-500 focus:translate-y-1"
         >
           Login
